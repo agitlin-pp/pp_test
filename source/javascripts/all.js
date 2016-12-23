43abfc6ebae07ab50a2ed1a2ccc919ab44f2a7aa
agitlin-pp <agitlin@paperlesspost.com>
2016-12-23 15:32:33 -0500

    added some buttons with hover effects and a dropdown

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,27 @@
 // This is where it all goes :)
+
+function DropDown(el) {
+  this.dd = el;
+  this.initEvents();
+}
+DropDown.prototype = {
+  initEvents : function() {
+    var obj = this;
+
+    obj.dd.on('click', function(event){
+      $(this).toggleClass('active');
+      event.stopPropagation();
+    });
+  }
+}
+
+$(function() {
+
+  var dd = new DropDown( $('.dd-wrapper') );
+
+  $(document).click(function() {
+    // all dropdowns
+    $('.dd-wrapper').removeClass('active');
+  });
+
+});
